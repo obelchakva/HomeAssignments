ed9098552c5b097426c11fa146261374d6eb6a84
ShadeOfMorr <shcheka.d@gmail.com>
2024-10-13 11:38:13 +0300

    Update assignment2a.cpp

--- Assignment2a/assignment2a.cpp
+++ Assignment2a/assignment2a.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <filesystem>
+ /* В main не должна находиться внутренняя логика программы, это нужно разбивать на функции и классы */
 int main() {
 	std::ifstream infile("input.bin", std::ios::binary);
 	if (!infile) {
@@ -9,10 +10,16 @@ int main() {
 		return 1;
 	}
 	std::streamsize size = std::filesystem::file_size("input.bin");
+	 /* Если у тебя будет много такого, то не сможешь уследить за всей выделенной памятью, поэтому
+	  * следует научиться пользоваться умными указателями или использовать здесь контейнер из 
+     	  * стандартной библиотеки (например vector, у него есть метод data(), который вернет массив,
+     	  * который можно передать в read) */
 	int* array = new int[size / sizeof(int)];
 	infile.read(reinterpret_cast<char*>(array), size);
 	infile.close();
 	for (size_t i=0; i <size / sizeof(int) / 2; ++i) {
+		/* Ну нужно же в итоге менять местами отдельные байты, а ты четверки байтов меняешь.
+  		 * Массив должен быть типа char */
 		std::swap(array[i], array[size / sizeof(int) -1 -i]);
 	}
 	std::ofstream outfile("outfile.bin", std::ios::binary);
