57f395fc6b0b6f041aac75598ec877164043cbda
obelchakva <obelchakva@gmail.com>
2024-12-13 11:37:15 +0300

    Added heading

--- Assignment3/Prime.cpp
+++ Assignment3/Prime.cpp
@@ -1,3 +1,6 @@
+/* Obelchak Vyacheslav st129564@student.spbu.ru
+   Home assignment #3
+*/
 #include "Prime.h"
 
 Prime::Prime(const std::string& name, int powerLevel, int speed, const Weapon& weapon, bool canFly)
