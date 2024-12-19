90f23cef7b5985fbbc9de4fd0ca4588ccc7a25e3
obelchakva <obelchakva@gmail.com>
2024-12-19 15:59:02 +0300

    Changed main.cpp

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -2,19 +2,23 @@
    Home assignment #3
 */
 #include <iostream>
+#include <memory>
 #include "Transformer.h"
 #include "Autobot.h"
 #include "Decepticon.h"
 #include "Prime.h"
 
 int main() {
-	Weapon blaster("Blaster");
-	Autobot optimus("Optimus Prime", 100, 80, blaster, true);
-	Decepticon megatron("Megatron", 90, 75, blaster, {"Invisibility", "Flight"});
-	Prime ultraMagnus("Ultra Magnus", 95, 85, blaster, true);
+    Weapon blaster("Blaster");
+    auto alliance = std::make_shared<Alliance>("Cybertron");
 
-	std::cout << optimus.getName() << " Wielding: " << optimus.attack() << std::endl;
-	std::cout << megatron.getName() << " Wilding: " << megatron.attack() << std::endl;
-	std::cout << ultraMagnus.getName() << " Wielding: " << ultraMagnus.attack() << std::endl;
-	return 0;
+    Autobot optimus("Optimus Prime", 100, 80, blaster, true, alliance);
+    Decepticon megatron("Megatron", 90, 75, blaster, {"Invisibility", "Flight"}, alliance);
+    Prime ultraMagnus("Ultra Magnus", 95, 85, blaster, true, alliance);
+
+    std::cout << optimus.getName() << " Wielding: " << optimus.attack() << std::endl;
+    std::cout << megatron.getName() << " Wilding: " << megatron.attack() << std::endl;
+    std::cout << ultraMagnus.getName() << " Wielding: " << ultraMagnus.attack() << std::endl;
+
+    return 0;
 }
