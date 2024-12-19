2dc1016f581d2e9c13a5cbaff50a9244965815c3
obelchakva <obelchakva@gmail.com>
2024-12-19 16:08:14 +0300

    Changed Decepticon.h

--- Assignment3/Decepticon.h
+++ Assignment3/Decepticon.h
@@ -1,23 +1,21 @@
 /* Obelchak Vyacheslav st129564@student.spbu.ru
    Home assignment #3
 */
-#ifndef DECEPTICONE_H
-#define DECEPTICONE_H
+#ifndef DECEPTICON_H
+#define DECEPTICON_H
 
 #include "Transformer.h"
 #include <vector>
 
 class Decepticon : public Transformer {
 public:
-	Decepticon(const std::string& name, int powerLevel, int speed, const Weapon& weapon, const std::vector<std::string>&
-abilities);
+    Decepticon(const std::string& name, int powerLevel, int speed, const Weapon& weapon, const std::vector<std::string>& abilities, std::shared_ptr<Alliance> alliance);
 
-	std::vector<std::string> getSpecialAbilities() const;
-	std::string attack() const override;
+    std::vector<std::string> getSpecialAbilities() const;
+    std::string attack() const override;
 
 private:
-	std::vector<std::string> specialAbilities_;
-
+    std::vector<std::string> specialAbilities_;
 };
 
 #endif
