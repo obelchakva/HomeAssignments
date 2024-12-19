f01d6a6a5732d291150030b9891f212dc1510bd3
obelchakva <obelchakva@gmail.com>
2024-12-19 16:04:39 +0300

    Changed Transformer.h

--- Assignment3/Transformer.h
+++ Assignment3/Transformer.h
@@ -5,35 +5,36 @@
 #define TRANSFORMER_H
 
 #include <string>
-
-class Weapon {
-public:
-	Weapon(const std::string& name) : name_(name) {}
-	std::string getName() const { return name_;}
-
-private:
-	std::string name_;
-};
+#include <memory>
+#include "Weapon.h"
+#include "Alliance.h"
 
 class Transformer {
 public:
-	Transformer(const std::string& name, int powerLevel, int speed,
-const Weapon& weapon);
-	virtual ~Transformer();
+    Transformer(const std::string& name, int powerLevel, int speed, const Weapon& weapon, std::shared_ptr<Alliance> alliance);
+    ~Transformer(); // Добавлены методы для weapon_, поле для ассоциации alliance_
 
-	std::string getName() const;
-	int getPowerLevel() const;
-	int getSpeed() const;
+    std::string getName() const;
+    int getPowerLevel() const;
+    int getSpeed() const;
+    const Weapon& getWeapon() const;
+    std::shared_ptr<Alliance> getAlliance() const;
 
-	void setPowerLevel(int power);
-	void setSpeed(int speed);
-	virtual std::string attack() const;
+    void setPowerLevel(int power);
+    void setSpeed(int speed);
+    void setWeapon(const Weapon& weapon); // Добалены методы для weapon_
+    void setAlliance(std::shared_ptr<Alliance> alliance); // Методы для работы с полем alliance_
 
-private:
-	std::string name_;
-	int powerLevel_;
-	int speed_;
-	Weapon weapon_;
+    virtual std::string attack() const;
+    virtual void transform() const; // Новый метод
+    void displayInfo() const; // Новый метод
+
+protected:
+    std::string name_;
+    int powerLevel_;
+    int speed_;
+    Weapon weapon_;
+    std::shared_ptr<Alliance> alliance_; // Методы для работы с полем alliance_
 };
 
 #endif
