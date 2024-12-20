60a490e7bef1ed06ddb13e90f4e22c9ee6c1d719
obelchakva <obelchakva@gmail.com>
2024-12-20 16:18:55 +0300

    Changed Autobot.cpp

--- Assignment4/Autobot.cpp
+++ Assignment4/Autobot.cpp
@@ -2,6 +2,7 @@
    Home assignment #4
 */
 #include "Autobot.h"
+#include <iomanip> // Для setw
 
 Autobot::Autobot(const std::string& name, int powerLevel, int speed, const Weapon& weapon, bool canFly, std::shared_ptr<Alliance> alliance)
     : Transformer(name, powerLevel, speed, weapon, alliance), canFly_(canFly) {}
@@ -16,4 +17,25 @@ void Autobot::setCanFly(bool canFly) {
 
 std::string Autobot::attack() const {
     return Transformer::attack() + " (Autobot)";
+}
+
+std::ostream& operator<<(std::ostream& os, const Autobot& a) {
+    os << static_cast<const Transformer&>(a); // Вызываем оператор << базового класса
+    os << "Can Fly: " << std::boolalpha << a.canFly_ << "\n"; // Добавляем дополнительную информацию
+    return os;
+}
+
+bool Autobot::operator<(const Autobot& other) const {
+    // Если Autobots могут летать, добавляем бонус к сравнению
+    int bonus = canFly_ ? 10 : 0;
+    return (powerLevel_ + speed_ + bonus) < (other.powerLevel_ + other.speed_ + (other.canFly_ ? 10 : 0));
+}
+
+bool Autobot::operator>(const Autobot& other) const {
+    int bonus = canFly_ ? 10 : 0;
+    return (powerLevel_ + speed_ + bonus) > (other.powerLevel_ + other.speed_ + (other.canFly_ ? 10 : 0));
+}
+
+bool Autobot::operator==(const Autobot& other) const {
+    return (powerLevel_ == other.powerLevel_) && (speed_ == other.speed_) && (canFly_ == other.canFly_);
 }
