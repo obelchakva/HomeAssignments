a109a1a6f01336956ba8b7742a5ef521f3d7eeb7
obelchakva <obelchakva@gmail.com>
2024-12-20 16:20:10 +0300

    Changed

--- Assignment4/Prime.cpp
+++ Assignment4/Prime.cpp
@@ -2,10 +2,32 @@
    Home assignment #4
 */
 #include "Prime.h"
+#include <iomanip> // Для setw
 
 Prime::Prime(const std::string& name, int powerLevel, int speed, const Weapon& weapon, bool canFly, std::shared_ptr<Alliance> alliance)
     : Autobot(name, powerLevel, speed, weapon, canFly, alliance) {}
 
 std::string Prime::attack() const {
     return Autobot::attack() + " (Prime)";
+}
+
+std::ostream& operator<<(std::ostream& os, const Prime& p) {
+    os << static_cast<const Autobot&>(p); // Вызываем оператор << родительского класса
+    os << "Title: Prime\n"; // Добавляем дополнительную информацию
+    return os;
+}
+
+bool Prime::operator<(const Prime& other) const {
+    // Принимаем во внимание статус Prime
+    int bonus = 20; // Допустимый бонус за звание Prime
+    return (powerLevel_ + speed_ + bonus) < (other.powerLevel_ + other.speed_ + 20);
+}
+
+bool Prime::operator>(const Prime& other) const {
+    int bonus = 20;
+    return (powerLevel_ + speed_ + bonus) > (other.powerLevel_ + other.speed_ + 20);
+}
+
+bool Prime::operator==(const Prime& other) const {
+    return (powerLevel_ == other.powerLevel_) && (speed_ == other.speed_);
 }
--- Assignment4/Prime.h
+++ Assignment4/Prime.h
@@ -10,6 +10,14 @@ class Prime : public Autobot {
 public:
     Prime(const std::string& name, int powerLevel, int speed, const Weapon& weapon, bool canFly, std::shared_ptr<Alliance> alliance);
     std::string attack() const override;
+
+    // Переопределяем оператор << для вывода дополнительной информации
+    friend std::ostream& operator<<(std::ostream& os, const Prime& p);
+
+    // Переопределяем операторы сравнения
+    bool operator<(const Prime& other) const;
+    bool operator>(const Prime& other) const;
+    bool operator==(const Prime& other) const;
 };
 
 #endif
